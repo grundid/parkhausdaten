115055e9c7f9c5d6de7ced13eb8f53139794923b
Adrian Stabiszewski <as@grundid.de>
2017-09-13 13:42:27 +0200

    run unlimited

--- scraper.js
+++ scraper.js
@@ -4,16 +4,29 @@ const scr = require('./scrape');
 const inputUrl = process.env.MORPH_INPUT_URL;
 const outputUrl = process.env.MORPH_OUTPUT_URL;
 
-request(inputUrl, (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-        const result = scr.scrape(html);
-        let opts = {
-            url: outputUrl,
-            method: 'POST',
-            json: result
-        };
-        request(opts, (err, res, body) => {
-            console.log("Done");
-        })
-    }
-});
+function scrapeData() {
+    console.log("Running update");
+    request(inputUrl, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const result = scr.scrape(html);
+            let opts = {
+                url: outputUrl,
+                method: 'POST',
+                json: result
+            };
+            request(opts, (err, res, body) => {
+                console.log("Done");
+            })
+        }
+    });
+}
+
+if (inputUrl && outputUrl) {
+    scrapeData();
+    setInterval(scrapeData, 6 * 60 * 1000);
+} else {
+    console.log("Config missing: ", inputUrl, " out: ", outputUrl);
+}
+
+
+
